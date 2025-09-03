3fdc630843b8250cd09e03555f7b7802842d084d
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2025-09-03 22:05:04 +0200

    Update frontend config to use deployed backend URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export const config = {
-  API_BASE: 'http://localhost:8080',
+  API_BASE: process.env.NODE_ENV === 'production' 
+    ? 'https://smartrecipe-generator-be.onrender.com' 
+    : 'http://localhost:8080',
   DEMO_USER_ID: 1  // This will be the demo user created on startup
 };
